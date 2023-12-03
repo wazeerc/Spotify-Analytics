83349f95edd8493e7f5c3bca775c66a6fca2197a
wazeerc <58399263+wazeerc@users.noreply.github.com>
2023-12-03 17:36:31 +0400

    code clean up pt.1

--- src/spotify-data-analyser.py
+++ src/spotify-data-analyser.py
@@ -8,10 +8,11 @@ class SpotifyDataAnalyser:
     def __init__(self):
         prompter = Prompter()
         self.is_extended_data = prompter.prompt_for_extended_data()
-        self.datasets_count = None
+        self.default_path = 'data/streaming'
+        self.extended_path = 'data/streaming_extended'
+        self.dataset_path = Path(self.extended_path) if self.is_extended_data else Path(self.default_path)
 
-        self.dataset_path = Path(
-            'data/streaming_extended') if self.is_extended_data else Path('data/streaming')
+        self.datasets_count = None
         if not self.is_extended_data:
             self.datasets_count = prompter.prompt_for_dataset_count()
 
@@ -21,8 +22,11 @@ class SpotifyDataAnalyser:
         self.data_analyser = DataAnalyser(self.data, self.is_extended_data)
 
     def main(self):
-        AnalyticsGenerator(self.data).execute_spotify_data_analyser(
-            self.is_extended_data, self.datasets_count, self.results_amount, self.dataset_path)
+        try:
+            AnalyticsGenerator(self.data).execute_spotify_data_analyser(
+                self.is_extended_data, self.datasets_count, self.results_amount, self.dataset_path)
+        except (Exception) as err:
+            print(f"Error :{err}")
 
 
 class Prompter:
@@ -33,7 +37,7 @@ class Prompter:
         self.results_amount = None
         self.max_results_amount = 100
 
-    def prompt_for_input(self, prompt_message, error_message, condition):
+    def prompt(self, prompt_message, error_message, condition):
         print(prompt_message)
         while True:
             user_input = input().lower()
@@ -42,7 +46,7 @@ class Prompter:
             print(error_message)
 
     def prompt_for_extended_data(self):
-        self.is_extended_data = self.prompt_for_input(
+        self.is_extended_data = self.prompt(
             'Do you want to fetch extended data? (y/n)',
             "Invalid choice. Please enter 'y' or 'n'.",
             lambda x: x in ['y', 'yes', 'n', 'no']
@@ -50,7 +54,7 @@ class Prompter:
         return self.is_extended_data == 'y' or self.is_extended_data == 'yes'
 
     def prompt_for_dataset_count(self):
-        self.datasets_count = self.prompt_for_input(
+        self.datasets_count = self.prompt(
             f'How many datasets do you want to analyze? ({
                 self.datasets_range.start}-{self.datasets_range.stop - 1})',
             f"Invalid choice. Please enter a number between {
@@ -60,7 +64,7 @@ class Prompter:
         return int(self.datasets_count)
 
     def prompt_for_results_amount(self):
-        self.results_amount = self.prompt_for_input(
+        self.results_amount = self.prompt(
             f'How many results do you want to display? (1-{
                 self.max_results_amount})',
             f'Invalid choice. Please enter a number between 1 and {
@@ -79,9 +83,9 @@ class DataLoader:
         if is_extended_data:
             return self.load_extended_data(dataset_path)
         else:
-            return self.load_non_extended_data(datasets_count, dataset_path)
+            return self.load_default_data(datasets_count, dataset_path)
 
-    def load_non_extended_data(self, datasets_count, dataset_path):
+    def load_default_data(self, datasets_count, dataset_path):
         for i in range(datasets_count):
             file_path = dataset_path / f'StreamingHistory{i}.json'
             if file_path.exists():
@@ -103,8 +107,12 @@ class DataLoader:
                     with open(file_path, 'r', encoding='utf-8') as file:
                         data = json.load(file)
                         for item in data:
-                            if item['master_metadata_track_name'] is not None and item['master_metadata_album_artist_name'] is not None and item['master_metadata_album_album_name'] is not None:
-                                yield item['master_metadata_track_name'], item['master_metadata_album_artist_name'], item['master_metadata_album_album_name']
+                            if (item['master_metadata_track_name'] is not None and
+                                item['master_metadata_album_artist_name'] is not None and
+                                    item['master_metadata_album_album_name'] is not None):
+                                yield (item['master_metadata_track_name'],
+                                       item['master_metadata_album_artist_name'],
+                                       item['master_metadata_album_album_name'])
                 except json.JSONDecodeError:
                     logging.error(f"\nCould not decode JSON from {file_path}.")
             else:
@@ -153,30 +161,30 @@ class AnalyticsGenerator:
         print(f"{'-' * 45}")
         print(f"{label.ljust(35)} | Count")
         print(f"{'-' * 45}")
+
         for item, count in counts.most_common(results_amount):
             trimmed_item = (item[:24] + '...') if len(item) > 35 else item
             print(f"{trimmed_item.ljust(35)} | {count}")
         print(f"{'-' * 45}\n")
 
     def execute_spotify_data_analyser(self, is_extended_data, datasets_count, results_amount, dataset_path):
-        try:
-            data = list(self.data.load_data(
-                is_extended_data, datasets_count, dataset_path))
-            self.data_analyser = DataAnalyser(data, is_extended_data)
-            print(f"\nAnalyzing {len(data)} tracks...\n")
+        data = list(self.data.load_data(
+            is_extended_data, datasets_count, dataset_path))
+        self.data_analyser = DataAnalyser(data, is_extended_data)
+
+        print(f"\nAnalyzing {len(data)} tracks...\n")
+
+        self.display_analytics(
+            self.data_analyser.get_most_played_tracks(), results_amount, "Track")
+        self.display_analytics(
+            self.data_analyser.get_most_listened_artists(), results_amount, "Artist")
+        if is_extended_data:
             self.display_analytics(
-                self.data_analyser.get_most_played_tracks(), results_amount, "Track")
-            self.display_analytics(
-                self.data_analyser.get_most_listened_artists(), results_amount, "Artist")
-            if is_extended_data:
-                self.display_analytics(
-                    self.data_analyser.get_most_listened_albums(), results_amount, "Album")
-        except (Exception) as e:
-            print(f'\n{e}')
+                self.data_analyser.get_most_listened_albums(), results_amount, "Album")
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.ERROR)
-    logging.info("Starting Spotify Data Analyser...")
+    logging.info(f"Starting Spotify Data Analyser...")
     spotify_data_analyser = SpotifyDataAnalyser().main()
-    logging.info("Spotify Data Analyser analysed and data displayed.")
+    logging.info(f"Spotify Data Analyser analysed and data displayed.")
